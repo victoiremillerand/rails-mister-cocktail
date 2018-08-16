8f8dbec23f463db3ee63e782722395d631270952
victoire millerand <vmillerand@gmail.com>
2018-08-16 15:58:44 +0200

    coktail

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,31 @@
+class DosesController < ApplicationController
+  def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = Dose.new(doses_params)
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render "new"
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    # @cocktail = Cocktail.find(params[:cocktail_id])
+    # @dose = Dose.all.where(cocktail: @cocktail)
+    @dose.delete
+    redirect_to cocktail_path(@dose.cocktail)
+  end
+
+  private
+
+  def doses_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
+end
