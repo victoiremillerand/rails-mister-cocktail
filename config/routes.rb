7575d1898227547ee885b87682c7f57faaed23d5
victoire millerand <vmillerand@gmail.com>
2018-08-17 12:29:46 +0200

    avec novuelle route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: "cocktails#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :cocktails, only: [:index, :new, :create, :show] do
     resources :doses, only: [:new,  :create]
